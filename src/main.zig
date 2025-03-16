65c9a3f8de4978f041c982e7528724ebbd4c5373
997volt <phgit@pm.me>
2025-03-16 10:26:03 +0100

    add array and integer cast

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,13 @@ pub fn main() void {
 
 test "use other module" {
     const int_const = @as(i32, 50);
-    var int_var = @as(i32, 50);
+    var int_var = @as(i32, 45);
     int_var = int_var + int_const;
+
+    const array = [_]u8{ 'h', 'e', 'l', 'l', 'o' };
+    const len = array.len; // 5
+    const int_len = @as(i32, @intCast(len));
+    int_var = int_var + int_len;
+
     try std.testing.expectEqual(@as(i32, 150), lib.add(int_const, int_var));
 }
